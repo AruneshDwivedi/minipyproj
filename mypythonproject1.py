e674bb2b3e927c7087bc1e033d8db34934c5b88a
AruneshDwivedi <kushaldwivedi1@gmail.com>
2024-11-26 10:56:21 +0000

    adding my first project

---
+++ mypythonproject1.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+data =input("please enter your goal and deadline date\n")
+input_data=data.split(":")
+
+goal=input_data[0]
+deadline=input_data[1]
+
+
+deadline_data=datetime.strptime(deadline,"%d/%m/%Y")
+today_data=datetime.today()
+
+time_left=deadline_data-today_data
+
+print(f"number of days remaining to complete your goal {goal} is {time_left.days} days.")
